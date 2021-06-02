8c3307d666ae30bd91b17096b5e40e0df8b5e4fd
zhouxianggang <841577282@qq.com>
2021-06-02 08:54:13 +0800

    master分支

--- oauth_auth/src/main/java/com/zxg/oauth_auth/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.zxg.oauth_auth.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-}
--- oauth_common/src/main/java/com/zxg/oauth_common/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zxg.oauth_common.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zxg.oauth_common.data.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
